75899cafb80b18b1234970c5ee3ee6e28fe5b4b1
Kev Comedia <kevcomedia@gmail.com>
2022-03-12 22:16:13 +0800

    Add id for drum machine

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import Display from './components/Display'
 
 const App = () => {
   return (
-    <div className="w-128 border-8 border-cyan-700 rounded-lg p-8 bg-cyan-500 grid grid-cols-6 grid-rows-3 gap-4">
+    <div
+      className="w-128 border-8 border-cyan-700 rounded-lg p-8 bg-cyan-500 grid grid-cols-6 grid-rows-3 gap-4"
+      id="drum-machine"
+    >
       <Button label="Q" />
       <Button label="W" />
       <Button label="E" />
