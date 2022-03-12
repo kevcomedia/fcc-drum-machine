5e8a69585d7cdce772117e30ca5bf37e88026331
Kev Comedia <kevcomedia@gmail.com>
2022-03-12 22:50:37 +0800

    Move audio elements to Button
    Doesn't make sense IMO, but the tests can't pass otherwise.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import DrumPad from './components/DrumPad'
+import Button from './components/Button'
 import Display from './components/Display'
 
 const App = () => {
@@ -7,44 +7,44 @@ const App = () => {
       className="w-128 border-8 border-cyan-700 rounded-lg p-8 bg-cyan-500 grid grid-cols-6 grid-rows-3 gap-4"
       id="drum-machine"
     >
-      <DrumPad
+      <Button
         label="Q"
         audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
       />
-      <DrumPad
+      <Button
         label="W"
         audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
       />
-      <DrumPad
+      <Button
         label="E"
         audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
       />
-      <DrumPad
+      <Button
         className="row-start-2"
         label="A"
         audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
       />
-      <DrumPad
+      <Button
         className="row-start-2"
         label="S"
         audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
       />
-      <DrumPad
+      <Button
         className="row-start-2"
         label="D"
         audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
       />
-      <DrumPad
+      <Button
         className="row-start-3"
         label="Z"
         audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
       />
-      <DrumPad
+      <Button
         className="row-start-3"
         label="X"
         audioSrc="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
       />
-      <DrumPad
+      <Button
         className="row-start-3"
         label="C"
         audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,23 @@
-const Button = ({ label, className }) => {
+import { useRef } from 'react'
+
+const Button = ({ label, audioSrc, className }) => {
+  const audioRef = useRef(null)
+
+  const handleClick = () => {
+    audioRef.current.play()
+  }
+
   return (
     <button
-      className={`${className} bg-green-500 hover:bg-green-400 text-lg font-bold p-2 aspect-square border border-green-900 rounded shadow-button shadow-green-900 active:bg-yellow-500 active:border-yellow-900 active:translate-y-2 active:shadow-none`}
+      className={`${className} drum-pad bg-green-500 hover:bg-green-400 text-lg font-bold p-2 aspect-square border border-green-900 rounded shadow-button shadow-green-900 active:bg-yellow-500 active:border-yellow-900 active:translate-y-2 active:shadow-none`}
       type="button"
+      onClick={handleClick}
+      id={`button-${label}`}
     >
       {label}
+      {audioSrc && (
+        <audio ref={audioRef} className="clip" id={label} src={audioSrc} />
+      )}
     </button>
   )
 }
