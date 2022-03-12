f6eed9e7e32150e25cc6136383cdc1f8f432748e
Kev Comedia <kevcomedia@gmail.com>
2022-03-12 21:49:49 +0800

    Layout buttons properly

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ const App = () => {
   return (
     <div className="w-128 border-8 border-cyan-700 rounded-lg p-8 bg-cyan-500 grid grid-cols-6 grid-rows-3 gap-4">
       <Button label="Q" />
+      <Button label="W" />
+      <Button label="E" />
+      <Button className="row-start-2" label="A" />
+      <Button className="row-start-2" label="S" />
+      <Button className="row-start-2" label="D" />
+      <Button className="row-start-3" label="Z" />
+      <Button className="row-start-3" label="X" />
+      <Button className="row-start-3" label="C" />
     </div>
   )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
-const Button = ({ label }) => {
+const Button = ({ label, className }) => {
   return (
     <button
-      className="bg-green-500 hover:bg-green-400 text-lg font-bold p-2 aspect-square border border-green-900 rounded shadow-button shadow-green-900 active:bg-yellow-500 active:border-yellow-900 active:translate-y-2 active:shadow-none"
+      className={`${className} bg-green-500 hover:bg-green-400 text-lg font-bold p-2 aspect-square border border-green-900 rounded shadow-button shadow-green-900 active:bg-yellow-500 active:border-yellow-900 active:translate-y-2 active:shadow-none`}
       type="button"
     >
       {label}
