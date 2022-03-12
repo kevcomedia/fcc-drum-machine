29ac6f6313118034b70228a16d849318ddf03f88
Kev Comedia <kevcomedia@gmail.com>
2022-03-12 22:16:25 +0800

    Add id for display

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,6 +2,7 @@ const Display = ({ label, className }) => {
   return (
     <div
       className={`${className} flex justify-center items-center bg-stone-700 text-white shadow-inner shadow-stone-900 [text-shadow:0_0_0.25rem_white]`}
+      id="display"
     >
       {label}
     </div>
