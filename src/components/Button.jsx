f16edf791855fd93fd1899466e88ea34a136a230
Kev Comedia <kevcomedia@gmail.com>
2022-03-13 11:04:45 +0800

    Animate drum pads on key press

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 const Button = ({ label, audioSrc, className }) => {
+  const [activeClasses, setActiveClasses] = useState('')
   const audioRef = useRef(null)
 
   const playAudio = () => {
@@ -22,9 +23,31 @@ const Button = ({ label, audioSrc, className }) => {
     }
   })
 
+  useEffect(() => {
+    const keyDownHandler = (event) => {
+      if (event.keyCode == label.charCodeAt()) {
+        setActiveClasses(
+          'bg-yellow-500 border-yellow-900 translate-y-2 shadow-none'
+        )
+      }
+    }
+    const keyUpHandler = (event) => {
+      if (event.keyCode == label.charCodeAt()) {
+        setActiveClasses('')
+      }
+    }
+
+    window.addEventListener('keydown', keyDownHandler)
+    window.addEventListener('keyup', keyUpHandler)
+    return () => {
+      window.removeEventListener('keydown', keyDownHandler)
+      window.removeEventListener('keyup', keyUpHandler)
+    }
+  })
+
   return (
     <button
-      className={`${className} drum-pad bg-green-500 hover:bg-green-400 text-lg font-bold p-2 aspect-square border border-green-900 rounded shadow-button shadow-green-900 active:bg-yellow-500 active:border-yellow-900 active:translate-y-2 active:shadow-none`}
+      className={`${className} ${activeClasses} drum-pad bg-green-500 hover:bg-green-400 text-lg font-bold p-2 aspect-square border border-green-900 rounded shadow-button shadow-green-900 active:bg-yellow-500 active:border-yellow-900 active:translate-y-2 active:shadow-none`}
       type="button"
       onClick={playAudio}
       id={`button-${label}`}
