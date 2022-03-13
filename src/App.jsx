6f189317050b8a3f6220855b36d70ee364bd8ca5
Kev Comedia <kevcomedia@gmail.com>
2022-03-13 12:57:21 +0800

    Dynamically render buttons using drum machine data

--- src/App.jsx
+++ src/App.jsx
@@ -11,48 +11,19 @@ const App = () => {
       className="w-128 border-8 border-cyan-700 rounded-lg p-8 bg-cyan-500 grid grid-cols-6 grid-rows-3 gap-4"
       id="drum-machine"
     >
-      <Button
-        label="Q"
-        audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
-      />
-      <Button
-        label="W"
-        audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
-      />
-      <Button
-        label="E"
-        audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
-      />
-      <Button
-        className="row-start-2"
-        label="A"
-        audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
-      />
-      <Button
-        className="row-start-2"
-        label="S"
-        audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
-      />
-      <Button
-        className="row-start-2"
-        label="D"
-        audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
-      />
-      <Button
-        className="row-start-3"
-        label="Z"
-        audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
-      />
-      <Button
-        className="row-start-3"
-        label="X"
-        audioSrc="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
-      />
-      <Button
-        className="row-start-3"
-        label="C"
-        audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
-      />
+      {drumMachineData.map(({ buttonLabel, displayText, src }, index) => {
+        // Tailwind requires that class names are hardcoded in full strings.
+        const rowStart = ['row-start-1', 'row-start-2', 'row-start-3']
+        return (
+          <Button
+            label={buttonLabel}
+            audioSrc={src}
+            displayText={displayText}
+            className={rowStart[Math.floor(index / 3)]}
+            key={buttonLabel}
+          />
+        )
+      })}
       <Display
         className="col-start-5 row-start-2 col-span-2"
         label={displayText}
